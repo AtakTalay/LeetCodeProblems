82516a2781e0121483ece90f1d7f5a8622a0c4ea
AtakTalay <40243456+AtakTalay@users.noreply.github.com>
2022-05-03 02:01:06 +0300

    NumberOfGoodPairs

---
+++ NumberOfGoodPairs.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/number-of-good-pairs/submissions/   
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        map<int, int> elementPositions;
+        map<int, int>::iterator itr;
+        int total = 0;
+        for(int i = 0; i < nums.size(); i++){
+            itr = elementPositions.find(nums[i]);
+            if(itr != elementPositions.end()){
+                total += itr->second;
+                itr->second++;
+            }
+            else{
+                cout << ":(" << endl;
+                elementPositions.insert({nums[i], 1});
+            }
+        }
+        return total;
+    }
+};
