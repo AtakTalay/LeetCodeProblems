8e639458abe82a064562fc59b19ee4279642721c
AtakTalay <40243456+AtakTalay@users.noreply.github.com>
2022-08-04 01:23:26 +0300

    Create MyCalendar_I.cpp

---
+++ MyCalendar_I.cpp
@@ -0,0 +1,17 @@
+class MyCalendar {
+public:
+    vector<pair<int,int>> a;
+    MyCalendar() {
+
+    }
+
+    bool book(int start, int end) {
+        for(int i = 0; i < a.size(); i++){
+            if((a[i].first <= start && a[i].second > start) || (a[i].first < end && a[i].second > end) || (start <= a[i].first && end >= a[i].second)){
+                return false;
+            }
+        }
+        a.push_back({start, end});
+        return true;
+    }
+};
